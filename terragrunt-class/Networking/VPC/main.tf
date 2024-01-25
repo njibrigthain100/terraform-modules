f21a965adfe5c1751ec5781b672fb73f0ca8661f
Brigthain kargong <kbrigthain@gmail.com>
2024-01-25 01:58:43 -0500

    code complete with some issues

--- terragrunt-class/Networking/VPC/main.tf
+++ terragrunt-class/Networking/VPC/main.tf
@@ -11,7 +11,7 @@ locals {
     
   }
 }
-
+#Creating the vpc
 resource "aws_vpc" "customer-vpc" {
     cidr_block = var.cidr_block
     enable_dns_hostnames = true 
@@ -23,7 +23,47 @@ resource "aws_vpc" "customer-vpc" {
     )
   
 }
+#Creating the internet gateway
+resource "aws_internet_gateway" "customer-igw" {
+  vpc_id = aws_vpc.customer-vpc.id
+  
+  tags = merge(local.common_tags,
+    {
+        "Name" = "${var.Owner}-${var.Environment}-igw"
+    }
+    )
 
+}
+#Creating an eip for nat gateway 
+resource "aws_eip" "customer-eip" {
+    domain = "vpc" 
+     tags = merge(local.common_tags,
+    {
+        "Name" = "${var.Owner}-${var.Environment}-eip"
+    }
+    )
+
+}
+
+#Creating the nat gateway
+resource "aws_nat_gateway" "customer-nat_gw" {
+# The 0 in the below code means that we are allocating the nat gateway only to the first subnet
+  subnet_id = element(aws_subnet.customer-public-subnets.*.id, 0)
+  allocation_id = aws_eip.customer-eip.id 
+  # The dependency here is important as the nat gateway is depended on the public subnet
+  # And the elastic ip. So these resources have to be created first before the ngw
+  depends_on = [ 
+    aws_subnet.customer-public-subnets,
+    aws_eip.customer-eip
+   ]
+
+    tags = merge(local.common_tags,
+    {
+        "Name" = "${var.Owner}-${var.Environment}-nat_gw"
+    }
+    )
+}
+#Creating the private subnets
 resource "aws_subnet" "customer-private-subnets" {
     vpc_id = aws_vpc.customer-vpc.id
     availability_zone = var.private_az[count.index]
@@ -37,7 +77,7 @@ resource "aws_subnet" "customer-private-subnets" {
     )
 
 }
-
+#Creating the public subnets
 resource "aws_subnet" "customer-public-subnets" {
     vpc_id = aws_vpc.customer-vpc.id
     availability_zone = var.public_az[count.index]
@@ -53,4 +93,56 @@ resource "aws_subnet" "customer-public-subnets" {
     }
     )
 
+}
+#Creating the private route table
+resource "aws_route_table" "customer-private-rt" {
+  vpc_id = aws_vpc.customer-vpc.id  
+  
+   tags = local.common_tags
+}
+
+# Creating private route 
+
+resource "aws_route" "customer-private-route" {
+    route_table_id = aws_route_table.customer-private-rt.id
+    destination_cidr_block = "0.0.0.0/0" 
+    gateway_id = aws_nat_gateway.customer-nat_gw.id
+    depends_on = [ 
+        aws_eip.customer-eip,
+        aws_nat_gateway.customer-nat_gw
+    ]
+  
+}
+#Creating the public route table 
+resource "aws_route_table" "customer-public-rt" {
+  vpc_id = aws_vpc.customer-vpc.id  
+
+   tags = local.common_tags
+}
+
+#Creating the public route 
+
+resource "aws_route" "customer-public-route" {
+    route_table_id = aws_route_table.customer-private-rt.id
+    destination_cidr_block = "0.0.0.0/0" 
+    gateway_id = aws_internet_gateway.customer-igw.id
+    depends_on = [ 
+        aws_internet_gateway.customer-igw
+    ]
+  
+}
+#Creating private route table association 
+resource "aws_route_table_association" "customer-private-rt-association" {
+    route_table_id = aws_route_table.customer-private-rt.id
+    count = length(var.private_subnets_cidr)
+    subnet_id = element(aws_subnet.customer-private-subnets.*.id, count.index)
+  
+}
+
+#Creating public route table association 
+resource "aws_route_table_association" "customer-public-rt-association" {
+    route_table_id = aws_route_table.customer-public-rt.id
+    count = length(var.public_subnets_cidr)
+    subnet_id = element(aws_subnet.customer-private-subnets.*.id, count.index)
+  
 }
